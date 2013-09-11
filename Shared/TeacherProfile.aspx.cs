b799b1e157d47c7b9fd1965b5a742a0ca4358e3e
Syed Sarmad Sabih <sam_sarmad@hotmail.com>
2013-09-11 18:54:56 +0500

    TeacherProfile page code fixed

--- Shared/TeacherProfile.aspx.cs
+++ Shared/TeacherProfile.aspx.cs
@@ -12,32 +12,44 @@ public partial class Administration_TeacherProfile : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Request.QueryString["id"] != null)
-        {
-
-            //MembershipUser student = Membership.GetUser(Request.QueryString["id"].ToString());
-            //StudentEmail.Text = student.Email.ToString();
-            //StudentUserName.Text = student.UserName.ToString();
-            TeacherUserName.Text= Request.QueryString["id"].ToString();
-            DataView dvSql = (DataView)TeacherDataSource.Select(DataSourceSelectArguments.Empty);
-            foreach (DataRowView drvSql in dvSql)
-            {
-                TeacherName.Text = drvSql["FirstName"].ToString();
-                TeacherLastName.Text = drvSql["LastName"].ToString();
-                TeacherEducation.Text = drvSql["Education"].ToString();
-                TeacherContact.Text = drvSql["Contact"].ToString();
-                TeacherEmail.Text = drvSql["Email"].ToString();
-                TeacherDesignation.Text = drvSql["Designation"].ToString();
-            }
-
-        }
+      string userName;
+      //setting the userName variable based on query string or user name value in the session
+      if (Request.QueryString["id"] != null)
+      {
+        userName = Request.QueryString["id"].ToString();
+      }
+      else
+      {
+        userName = User.Identity.Name;
+      }
+          TeacherUserName.Text = userName;
+          DataView dvSql = (DataView)TeacherDataSource.Select(DataSourceSelectArguments.Empty);
+          foreach (DataRowView drvSql in dvSql)
+          {
+              TeacherName.Text = drvSql["FirstName"].ToString();
+              TeacherLastName.Text = drvSql["LastName"].ToString();
+              TeacherEducation.Text = drvSql["Education"].ToString();
+              TeacherContact.Text = drvSql["Contact"].ToString();
+              TeacherEmail.Text = drvSql["Email"].ToString();
+              TeacherDesignation.Text = drvSql["Designation"].ToString();
+          }
     }
 
     protected void TeacherDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
     {
-        MembershipUser teacher = Membership.GetUser(Request.QueryString["id"].ToString());
-        Guid teacherId = (Guid)teacher.ProviderUserKey;
-        //assign the currently logged on user's user id to the @userid parameter
-        e.Command.Parameters["@TeacherId"].Value = teacherId;
+      MembershipUser teacher;
+
+      if (Request.QueryString["id"] != null)
+      {
+        teacher = Membership.GetUser(Request.QueryString["id"].ToString());
+      }
+      // if user name is not present in the query string then look for it in the session then assigns it to the datasource parameter
+      else
+      {
+        teacher = Membership.GetUser(User.Identity.Name);
+      }
+      Guid teacherId = (Guid)teacher.ProviderUserKey;
+      //assign the currently logged on user's user id to the @userid parameter
+      e.Command.Parameters["@TeacherId"].Value = teacherId;
     }
 }
