773ff6f4ac5e22afdd82e1d288a18016c8bbde74
Syed Sarmad Sabih <sam_sarmad@hotmail.com>
2013-09-13 21:46:32 +0500

    use external class to send emails on registrations

--- Administration/Parents/AddParent.aspx.cs
+++ Administration/Parents/AddParent.aspx.cs
@@ -59,16 +59,7 @@ public partial class Administration_AddParent : System.Web.UI.Page
 
             Roles.AddUserToRole(AddNewParent.UserName, "parent");
             //Email
-            string recipient = Email.Text;
-            string from = "noreply@eims.com";
-            string subject = "Welcome to Educational Institute Management System";
-            string body = "Hi Mr." + FirstName.Text + " " + LastName.Text + ", you are now a registered user.\nUserName:" + UserName.Text + "\nPassword:" + Password.Text;
-            MailMessage objMail = new MailMessage(from, recipient, subject, body);
-            NetworkCredential objNC = new NetworkCredential("noreply.eims@live.com", "admin123");
-            SmtpClient objsmtp = new SmtpClient("smtp.live.com", 587); // for hotmail
-            objsmtp.EnableSsl = true;
-            objsmtp.Credentials = objNC;
-            objsmtp.Send(objMail);
+            EimsHelper.SendMail(Email.Text, "EIMS Registration.", "Hi Mr." + FirstName.Text + " " + LastName.Text + ".\nYou are added as a parent on Educational Institute Management System.\nYou can log in with these credentials:\nUsername: " + UserName.Text + "\nPassword: " + Password.Text + "\nWe hope to provide you a great experience.");
         }
 
     }
