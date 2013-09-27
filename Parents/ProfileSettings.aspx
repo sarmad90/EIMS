8b30f99508837765d087d95898349035d4254fd7
Syed Sarmad Sabih <sam_sarmad@hotmail.com>
2013-09-27 20:47:43 +0500

    Parent profile settings

--- Parents/ProfileSettings.aspx
+++ Parents/ProfileSettings.aspx
@@ -1,9 +1,35 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="ProfileSettings.aspx.cs" Inherits="Parents_ProfileSettings" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="LoginContent" Runat="Server">
-</asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+  <!--Parent Profile Settings-->
+  <div class="row">
+    <div class="span4"><asp:Image ID="DisplayPicture" runat="server" ImageUrl="~/img/graduated-icon_585.png" Height="200px" Width="200px" /></div>
+    <div class="span8">
+      <div class="row"><div class="span2"><b>First Name: </b></div><div class="span3"><asp:TextBox ID="TxtFirstName" runat="server"></asp:TextBox></div></div>
+      <div class="row"><div class="span2"><b>Last Name: </b></div><div class="span3"><asp:TextBox ID="TxtLastName" runat="server" required="true"></asp:TextBox></div></div>
+      <div class="row"><div class="span2"><b>Contact Number: </b></div><div class="span3"><asp:TextBox ID="TxtContact" runat="server" required="true"></asp:TextBox></div></div>
+      <div class="row"><div class="span2"><b>Email: </b></div><div class="span3"><asp:TextBox ID="TxtEmail" runat="server" required="true"></asp:TextBox></div></div>
+      <div class="row"><div class="span2"><b>Address: </b></div><div class="span3"><asp:TextBox ID="TxtAddress" runat="server" required="true"></asp:TextBox></div></div>
+      <div class="row"><div class="span2"><b>Gender: </b></div><div class="span3"><asp:DropDownList ID="DDGender" runat="server">
+        <asp:ListItem Selected="True">Male</asp:ListItem>
+        <asp:ListItem>Female</asp:ListItem>
+        </asp:DropDownList></div></div>
+      <div class="row"><div class="span2"><b>Display Picture: </b></div><div class="span3"><asp:FileUpload ID="FUDisplayPic" runat="server" /></div></div>
+      <div class="form-actions">
+        <div class="span2">
+          <asp:Button ID="UpdateProfile" CssClass="btn btn-primary" runat="server" Text="Update Profile" OnClick="UpdateProfile_Click" /></div>
+        <div class="span2"><asp:LinkButton ID="LinkButton2" runat="server" CssClass="btn" PostBackUrl='javascript:history.go(-1)'>Back</asp:LinkButton></div>
+      </div>
+    </div>
+  </div>
+  <asp:SqlDataSource ID="ParentDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:EIMSConnectionString %>" OnSelecting="ParentDataSource_Selecting" SelectCommand="SELECT aspnet_Membership.Email,
+ParentProfiles.FirstName,ParentProfiles.LastName, ParentProfiles.Contact,
+ParentProfiles.Address, ParentProfiles.Avatar,ParentProfiles.Gender
+FROM ParentProfiles
+INNER JOIN aspnet_Membership ON aspnet_Membership.UserId=ParentProfiles.ParentId
+WHERE (ParentProfiles.ParentId =@ParentId)">
+        <SelectParameters>
+            <asp:Parameter Name="ParentId" />
+        </SelectParameters>
+    </asp:SqlDataSource>
 </asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="SideNavigation" Runat="Server">
-</asp:Content>
-
--- Parents/ProfileSettings.aspx.cs
+++ Parents/ProfileSettings.aspx.cs
@@ -4,11 +4,76 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class Parents_ProfileSettings : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    if (!IsPostBack)
     {
-
+      DataView studentDataView = (DataView)ParentDataSource.Select(DataSourceSelectArguments.Empty);
+      foreach (DataRowView dvr in studentDataView)
+      {
+        TxtFirstName.Text = dvr["FirstName"].ToString();
+        TxtLastName.Text = dvr["LastName"].ToString();
+        TxtContact.Text = dvr["Contact"].ToString();
+        TxtEmail.Text = dvr["Email"].ToString();
+        TxtAddress.Text = dvr["Address"].ToString();
+        if (dvr["Gender"].ToString() == "Female")
+        {
+          DDGender.SelectedIndex = 1;
+        }
+        if (dvr["Avatar"].ToString() != "")
+        {
+          DisplayPicture.ImageUrl = dvr["Avatar"].ToString();
+        }
+      }
     }
+  }
+  protected void ParentDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+  {
+    MembershipUser parent = Membership.GetUser(User.Identity.Name);
+    Guid parentId = (Guid)parent.ProviderUserKey;
+    e.Command.Parameters["@ParentId"].Value = parentId;
+  }
+  protected void UpdateProfile_Click(object sender, EventArgs e)
+  {
+    MembershipUser parent = Membership.GetUser(User.Identity.Name);
+    Guid parentId = (Guid)parent.ProviderUserKey;
+    string connectionString = ConfigurationManager.ConnectionStrings["EIMSConnectionString"].ConnectionString;
+
+    string updateSql = "UPDATE ParentProfiles SET FirstName=@FirstName,LastName=@LastName,Contact=@Contact,Address=@Address,Gender=@Gender,Avatar=@Avatar WHERE ParentId=@ParentId";
+    string fileName;
+    string ext = System.IO.Path.GetExtension(this.FUDisplayPic.PostedFile.FileName);
+    fileName = Server.MapPath("~/img/ParentAvatars/Display_picture_" + User.Identity.Name + ext);
+    using (SqlConnection myConnection = new SqlConnection(connectionString))
+    {
+      myConnection.Open();
+      SqlCommand myCommand = new SqlCommand(updateSql, myConnection);
+      myCommand.Parameters.AddWithValue("@FirstName", TxtFirstName.Text);
+      myCommand.Parameters.AddWithValue("@LastName", TxtLastName.Text);
+      myCommand.Parameters.AddWithValue("@Contact", TxtContact.Text);
+      myCommand.Parameters.AddWithValue("@Address", TxtAddress.Text);
+      myCommand.Parameters.AddWithValue("@Gender", DDGender.SelectedValue);
+      myCommand.Parameters.AddWithValue("@ParentId", parentId);
+      if (FUDisplayPic.HasFile)
+      {
+        myCommand.Parameters.AddWithValue("@Avatar", "~/img/ParentAvatars/Display_picture_" + User.Identity.Name + ext);
+        FUDisplayPic.SaveAs(fileName);
+      }
+      else
+      {
+        myCommand.Parameters.AddWithValue("@Avatar", DisplayPicture.ImageUrl);
+      }
+      myCommand.ExecuteNonQuery();
+    }
+    parent.Email = TxtEmail.Text;
+    Membership.UpdateUser(parent);
+    Session["Notice"] = "Your Profile has been updated!";
+    Response.Redirect("~/parents/profilesettings.aspx");
+  }
 }
