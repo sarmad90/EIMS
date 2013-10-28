708c3ad984acb38fdb436c9827f979707c58c673
Syed Sarmad Sabih <sam_sarmad@hotmail.com>
2013-10-29 00:15:28 +0500

    Send email method

--- App_Code/EimsHelper.cs
+++ App_Code/EimsHelper.cs
@@ -19,16 +19,50 @@ public class EimsHelper
 
   public static void SendMail(string recipient, string subject, string body)
   {
-    //string recipient = Email.Text;
     string from = "noreply@eims.com";
     //string subject = "Welcome to Educational Institute Management System";
     //string body = "Hi Mr." + FirstName.Text + " " + LastName.Text + ", you are now a registered user.\nUserName:" + UserName.Text + "\nPassword:" + Password.Text;
     MailMessage objMail = new MailMessage(from, recipient, subject, body);
-    NetworkCredential objNC = new NetworkCredential("noreply.eims@live.com", "admin123");
-    SmtpClient objsmtp = new SmtpClient("smtp.live.com", 587); // for hotmail
-    objsmtp.EnableSsl = true;
-    objsmtp.Credentials = objNC;
-    objsmtp.Send(objMail);
+    //NetworkCredential objNC = new NetworkCredential("noreply.eims@live.com", "admin123");
+    SmtpClient smtpClient = new SmtpClient("localhost", 25);
+    smtpClient.UseDefaultCredentials = true;
+    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+    //smtpClient.EnableSsl = true;
+    smtpClient.Send(objMail);
+    //try
+    //{
+    //  //string recipient = Email.Text;
+    //  string from = "noreply@eims.com";
+    //  //string subject = "Welcome to Educational Institute Management System";
+    //  //string body = "Hi Mr." + FirstName.Text + " " + LastName.Text + ", you are now a registered user.\nUserName:" + UserName.Text + "\nPassword:" + Password.Text;
+    //  MailMessage objMail = new MailMessage(from, recipient, subject, body);
+    //  NetworkCredential objNC = new NetworkCredential("noreply.eims@live.com", "admin123");
+    //  SmtpClient objsmtp = new SmtpClient("smtp.live.com", 587); // for hotmail
+    //  objsmtp.ServicePoint.MaxIdleTime = 1;
+    //  objsmtp.EnableSsl = true;
+    //  objsmtp.Credentials = objNC;
+    //  objsmtp.Send(objMail);
+    //  objMail.Dispose();
+    //}
+    //catch
+    //{
+    //  string from = "noreply@eims.com";
+    //  //string subject = "Welcome to Educational Institute Management System";
+    //  //string body = "Hi Mr." + FirstName.Text + " " + LastName.Text + ", you are now a registered user.\nUserName:" + UserName.Text + "\nPassword:" + Password.Text;
+    //  MailMessage objMail = new MailMessage(from, recipient, subject, body);
+    //  NetworkCredential objNC = new NetworkCredential("noreply.eims@yahoo.com", "Admin123");
+    //  SmtpClient objsmtp = new SmtpClient("smtp.mail.yahoo.com", 465); // for yahoo
+    //  objsmtp.ServicePoint.MaxIdleTime = 1;
+    //  objsmtp.EnableSsl = true;
+    //  objsmtp.Credentials = objNC;
+    //  objsmtp.Send(objMail);
+    //  objMail.Dispose();
+    //}
+    //finally
+    //{
+      
+    //}
+    
   }
 
 }
