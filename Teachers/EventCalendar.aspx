46ef0a4e1e815c166b4eaf521654cc40a8c62113
Syed Sarmad Sabih <sam_sarmad@hotmail.com>
2013-11-17 02:43:52 +0500

    Event Calendar on teacher side

---
+++ Teachers/EventCalendar.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="EventCalendar.aspx.cs" Inherits="Students_EventCalendar" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+  <div class="row">
+    <div class="span10 offset1">
+      <asp:Calendar ID="EventCalendar" runat="server" BackColor="White" BorderColor="White" BorderWidth="1px" Font-Names="Verdana" Font-Size="9pt" ForeColor="Black" Height="450px" NextPrevFormat="FullMonth" Width="900px" OnDayRender="EventCalendar_DayRender" SelectionMode="None">
+        <DayHeaderStyle Font-Bold="True" Font-Size="8pt" />
+        <DayStyle BorderColor="Black" BorderStyle="None" BorderWidth="1px" />
+        <NextPrevStyle Font-Bold="True" Font-Size="8pt" ForeColor="#333333" VerticalAlign="Bottom" />
+        <OtherMonthDayStyle ForeColor="#999999" />
+        <SelectedDayStyle BackColor="#333399" ForeColor="White" />
+        <TitleStyle BackColor="White" BorderColor="Black" BorderWidth="4px" Font-Bold="True" Font-Size="12pt" ForeColor="#333399" />
+        <TodayDayStyle BackColor="#CCCCCC" />
+      </asp:Calendar>
+      <asp:SqlDataSource ID="AssignmentsDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:EIMSConnectionString %>" SelectCommand="select Assignments.SubmissionDate, Assignments.AssignmentId,Assignments.Title, Assignments.Description, Assignments.TotalMarks, Classes.ClassId,Courses.CourseName,(TeacherProfiles.FirstName + ' ' + TeacherProfiles.LastName) as TeacherName from Classes
+INNER JOIN TeacherProfiles ON Classes.TeacherId=TeacherProfiles.TeacherId
+INNER JOIN Courses ON Classes.CourseId=Courses.CourseId
+INNER JOIN Assignments ON Assignments.ClassId=Classes.ClassId
+WHERE TeacherProfiles.TeacherId = @TeacherId" OnSelecting="AssignmentsDataSource_Selecting">
+        <SelectParameters>
+          <asp:Parameter Name="TeacherId" />
+        </SelectParameters>
+      </asp:SqlDataSource>
+      <asp:SqlDataSource ID="QuizzesDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:EIMSConnectionString %>" SelectCommand="select Quizzes.QuizDate, Quizzes.QuizId,Quizzes.Title, Quizzes.Description, Quizzes.TotalMarks, Classes.ClassId,Courses.CourseName,(TeacherProfiles.FirstName + ' ' + TeacherProfiles.LastName) as TeacherName from Classes
+INNER JOIN TeacherProfiles ON Classes.TeacherId=TeacherProfiles.TeacherId
+INNER JOIN Courses ON Classes.CourseId=Courses.CourseId
+INNER JOIN Quizzes ON Quizzes.ClassId=Classes.ClassId
+WHERE TeacherProfiles.TeacherId = @TeacherId" OnSelecting="AssignmentsDataSource_Selecting">
+        <SelectParameters>
+          <asp:Parameter Name="TeacherId" />
+        </SelectParameters>
+      </asp:SqlDataSource>
+    </div>
+  </div>
+  <!-- Modal -->
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">Modal header</h3>
+  </div>
+  <div class="modal-body">
+    <b>Subject: </b>
+    <p id="course-modal"></p>
+    <b>Teacher: </b>
+    <p id="teacher-modal"></p>
+    <b>Quiz Date: </b>
+    <p id="date-modal"></p>
+    <b>Total Marks: </b>
+    <p id="marks-modal"></p>
+    <b>Description: </b>
+    <p id="body-modal">One fine body…</p>
+  </div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+  </div>
+</div>
+  <script type="text/javascript">
+    $(document).ready(function () {
+      $('.event').click(function () {
+        var linkId = $(this).attr("id");
+        
+        var title=$('#' + linkId + 'Title').val();
+        $('#myModalLabel').text(title);
+        var description = $('#' + linkId + 'Description').val();
+        $('#body-modal').text(description);
+        var date = $('#' + linkId + 'Date').val();
+        $('#date-modal').text(date);
+        var marks = $('#' + linkId + 'Marks').val();
+        $('#marks-modal').text(marks);
+        var teacher = $('#' + linkId + 'Teacher').val();
+        $('#teacher-modal').text(teacher);
+        var course = $('#' + linkId + 'Course').val();
+        $('#course-modal').text(course);
+      });
+    });
+  </script>
+</asp:Content>
---
+++ Teachers/EventCalendar.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Security;
+
+public partial class Students_EventCalendar : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void EventCalendar_DayRender(object sender, DayRenderEventArgs e)
+    {
+      DataView dvSql = (DataView)AssignmentsDataSource.Select(DataSourceSelectArguments.Empty);
+      foreach (DataRowView drvSql in dvSql)
+      {
+        DateTime date = (DateTime)drvSql["SubmissionDate"];
+        //Response.Write(date.ToShortDateString());
+        if (date.ToShortDateString() == e.Day.Date.ToShortDateString())
+        {
+          Literal l = new Literal();
+          l.Visible = true;
+          l.Text = "<br/>";
+          e.Cell.Controls.Add(l);
+          System.Text.StringBuilder modalLink = new System.Text.StringBuilder();
+          modalLink.AppendFormat("<a href=" + "#myModal" + " role=" + "button" + " class=" + "event" + " id=" + "Assignment" + drvSql["AssignmentId"] + " data-toggle=" + "modal" + ">" + "Assignment" + "</a>");
+          Literal l2 = new Literal();
+          l2.Visible = true;
+          l2.Text = modalLink.ToString();
+          e.Cell.Controls.Add(l2);
+          HiddenField hf = new HiddenField();
+          hf.Value = drvSql["Title"].ToString();
+          hf.ID = "Assignment" + drvSql["AssignmentId"] + "Title";
+          e.Cell.Controls.Add(hf);
+          HiddenField hf2 = new HiddenField();
+          hf2.Value = drvSql["Description"].ToString();
+          hf2.ID = "Assignment" + drvSql["AssignmentId"] + "Description";
+          e.Cell.Controls.Add(hf2);
+          HiddenField hf3 = new HiddenField();
+          hf3.Value = date.ToLongDateString();
+          hf3.ID = "Assignment" + drvSql["AssignmentId"] + "Date";
+          e.Cell.Controls.Add(hf3);
+          HiddenField hf4 = new HiddenField();
+          hf4.Value = drvSql["TotalMarks"].ToString();
+          hf4.ID = "Assignment" + drvSql["AssignmentId"] + "Marks";
+          e.Cell.Controls.Add(hf4);
+          HiddenField hf5 = new HiddenField();
+          hf5.Value = drvSql["TeacherName"].ToString();
+          hf5.ID = "Assignment" + drvSql["AssignmentId"] + "Teacher";
+          e.Cell.Controls.Add(hf5);
+          HiddenField hf6 = new HiddenField();
+          hf6.Value = drvSql["CourseName"].ToString();
+          hf6.ID = "Assignment" + drvSql["AssignmentId"] + "Course";
+          e.Cell.Controls.Add(hf6);
+          //Label lb = new Label();
+          //lb.Visible = true;
+          //lb.ForeColor = System.Drawing.Color.Blue;
+          ////lb.Text = drvSql["Title"].ToString();
+          //lb.Text = "Assignment";
+          //string a = lb.Text;
+          //e.Cell.Controls.Add(lb);
+        }
+      }
+
+
+      DataView dvSql2 = (DataView)QuizzesDataSource.Select(DataSourceSelectArguments.Empty);
+      foreach (DataRowView drvSql in dvSql2)
+      {
+        DateTime date = (DateTime)drvSql["QuizDate"];
+        //Response.Write(date.ToShortDateString());
+        if (date.ToShortDateString() == e.Day.Date.ToShortDateString())
+        {
+          Literal l = new Literal();
+          l.Visible = true;
+          l.Text = "<br/>";
+          e.Cell.Controls.Add(l);
+          //HyperLink hl = new HyperLink();
+          //hl.Visible = true;
+          System.Text.StringBuilder modalLink = new System.Text.StringBuilder();
+          modalLink.AppendFormat("<a href=" + "#myModal" + " role=" + "button"+ " class="+ "event" + " id=" + "Quiz"+drvSql["QuizId"]  +" data-toggle=" + "modal" +">" + "Quiz" + "</a>");
+          Literal l2 = new Literal();
+          l2.Visible=true;
+          l2.Text = modalLink.ToString();
+          e.Cell.Controls.Add(l2);
+          HiddenField hf = new HiddenField();
+          hf.Value = drvSql["Title"].ToString();
+          hf.ID = "Quiz" + drvSql["QuizId"]+"Title";
+          e.Cell.Controls.Add(hf);
+          HiddenField hf2 = new HiddenField();
+          hf2.Value = drvSql["Description"].ToString();
+          hf2.ID = "Quiz" + drvSql["QuizId"] + "Description";
+          e.Cell.Controls.Add(hf2);
+          HiddenField hf3 = new HiddenField();
+          hf3.Value = date.ToLongDateString();
+          hf3.ID = "Quiz" + drvSql["QuizId"] + "Date";
+          e.Cell.Controls.Add(hf3);
+          HiddenField hf4 = new HiddenField();
+          hf4.Value = drvSql["TotalMarks"].ToString();
+          hf4.ID = "Quiz" + drvSql["QuizId"] + "Marks";
+          e.Cell.Controls.Add(hf4);
+          HiddenField hf5 = new HiddenField();
+          hf5.Value = drvSql["TeacherName"].ToString();
+          hf5.ID = "Quiz" + drvSql["QuizId"] + "Teacher";
+          e.Cell.Controls.Add(hf5);
+          HiddenField hf6 = new HiddenField();
+          hf6.Value = drvSql["CourseName"].ToString();
+          hf6.ID = "Quiz" + drvSql["QuizId"] + "Course";
+          e.Cell.Controls.Add(hf6);
+          //hl.ForeColor = System.Drawing.Color.Purple;
+          //lb.Text = drvSql["Title"].ToString();
+          //hl.Text = "Quiz";  
+          
+          //e.Cell.Controls.Add(hl);
+        }
+      }
+    }
+    protected void AssignmentsDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+      MembershipUser teacher = Membership.GetUser(User.Identity.Name);
+      Guid teacherId = (Guid)teacher.ProviderUserKey;
+      e.Command.Parameters["@TeacherId"].Value = teacherId;
+    }
+    
+}
